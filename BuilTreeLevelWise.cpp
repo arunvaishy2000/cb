f98098157284e5c8eaae2b52cf2821dce18a5520
arunvaishy2000 <arunvaishy2000@gmail.com>
2019-10-22 19:41:05 +0530

    Build Tree By level wise cb

---
+++ BuilTreeLevelWise.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<queue>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node*left;
+	node*right;
+
+	node(int d){
+		data = d;
+		left = right = NULL;
+	}
+};
+void print(node*root){
+	if(root==NULL){
+		return;
+	}
+	cout<<root->data<<" ";
+	print(root->left);
+	print(root->right);
+}
+node* buildTreeLevelWise(){
+
+	int d;
+	cin>>d;
+
+	node*root = new node(d);
+	queue<node*> q;
+	q.push(root);
+
+	while(!q.empty()){
+
+		node*f = q.front();
+		q.pop();
+		int c1,c2;
+		cin>>c1>>c2;
+
+		if(c1!=-1){
+			f->left = new node(c1);
+			q.push(f->left);
+		}
+		if(c2!=-1){
+			f->right = new node(c2);
+			q.push(f->right);
+		}
+	}
+	return root;
+}
+int main()
+{
+    node* root;
+    root = buildTreeLevelWise();
+    print(root);
+    return 0 ;
+}
